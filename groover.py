4f4c664e23d1b32124efaa34175b0a4108603aa5
Bennett Levine <ben.lvn@gmail.com>
2018-03-26 00:33:50 -0400

    Created Master and Groover classes

---
+++ groover.py
@@ -0,0 +1,13 @@
+class Groover:
+
+	name = ""
+
+	# Number of tickets in possession
+	student_tix = 0
+	adult_tix = 0
+
+	# Number of tickets sold
+	agg_student = 0
+	agg_adult = 0
+
+
---
+++ master.py
@@ -0,0 +1,14 @@
+class Master:
+
+	# Amt currently in posetion
+	student_tix = 0
+	adult_tix = 0
+
+	# Amt given this round
+	total_student = 0
+	total_adult = 0
+
+	# List of Groover objects puppets under
+	# control of the Master
+	groovers = []
+
